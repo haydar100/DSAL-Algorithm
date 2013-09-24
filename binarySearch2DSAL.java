11fefbf5ef1c645d332d2a6fe0fbdcb370ab3cae
haydar100 <haydar.yilmaz@gmail.com>
2013-09-24 21:38:05 +0200

    BinarySearch2 uploaded

---
+++ binarySearch2DSAL.java
@@ -0,0 +1,32 @@
+
+
+public class binarySearch2DSAL<T> {
+	   int binarySearch2( Comparable<T>[ ] a, int low, int high,T x )
+	    {
+	    
+	        int mid;
+
+	        if( low <= high )
+	        {
+	            mid = ( low + high ) / 2;
+	            System.out.println("mid: " + mid);
+
+	            if( a[mid].compareTo( x ) < 0 )
+	            	  return binarySearch2(a, mid + 1, high, x);
+	            else if( a[mid].compareTo( x ) > 0 )
+	            	  return binarySearch2(a, mid - 1, high, x);
+	            else
+	                return mid;
+	        }
+	        return -1;
+
+	    }
+	   
+	   int binarySearch2( Comparable<T>[ ] a, T x ) // zoek element x in 'a'
+	   {
+		   return binarySearch2( a, 0, a.length-1, x );
+	   }
+	
+	
+	
+}
